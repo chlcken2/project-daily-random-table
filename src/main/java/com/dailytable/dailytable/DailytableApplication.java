3ea6db112f53df402219e944dfbf6efc3f54d0ab
창현 <changhyeon@changhyeon-ui-MacBookPro.local>
2026-02-20 09:52:03 +0900

    first commit

---
+++ src/main/java/com/dailytable/dailytable/DailytableApplication.java
@@ -0,0 +1,13 @@
+package com.dailytable.dailytable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailytableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailytableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailytable/dailytable/DailytableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailytable.dailytable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailytableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
