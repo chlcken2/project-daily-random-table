b7551c823f54732a18d17e3e595ad900932fa892
changhyeon <ckdgus988@gmail.com>
2026-03-02 22:01:19 +0900

    feat: BaseExcpetion 정의

---
+++ src/main/java/com/dailytable/dailytable/global/exception/BaseException.java
@@ -0,0 +1,20 @@
+package com.dailytable.dailytable.global.exception;
+
+
+import com.dailytable.dailytable.global.common.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BaseException(ErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode(){
+        return errorCode;
+    }
+}
