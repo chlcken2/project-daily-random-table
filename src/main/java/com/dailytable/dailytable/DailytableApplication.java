b6f514fa1fe4a39def0a618548d0e109b3447baf
AN-ILGWON <jindohound@gmail.com>
2026-03-06 09:20:13 +0900

    test: git push pull Request test

--- src/main/java/com/dailytable/dailytable/DailytableApplication.java
+++ src/main/java/com/dailytable/dailytable/DailytableApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DailytableApplication {
-//DailytableApplication
 	public static void main(String[] args) {
 		SpringApplication.run(DailytableApplication.class, args);
 	}
-
 }
