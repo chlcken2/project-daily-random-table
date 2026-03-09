8df6e61782bd4898026c8d9211c732bef6632147
Yuya <Yuya@Yuya>
2026-03-09 10:38:01 +0900

    상세페이지 관련 DTO 생성

---
+++ src/main/java/com/dailytable/dailytable/domain/recipe/RecipeDetailDto.java
@@ -0,0 +1,35 @@
+package com.dailytable.dailytable.domain.recipe;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.dailytable.dailytable.domain.ingredient.IngredientDto;
+
+import lombok.Data;
+
+@Data
+public class RecipeDetailDto {
+	private Long id;
+	private Long userId;
+	private String title;
+	private String titleImage;
+	private String description;
+	private int cookingTime;
+    private String difficultyLabel;
+	private String difficultyId;
+	private String createdAtFormatted;
+	private int cuisineId;
+	private boolean isAiGenerated;
+	private boolean isPublic;
+	private int viewCount;
+	private int commentCount;
+	private int likeCount;
+	private int popularityScore;
+	private boolean isLiked;
+	private List<RecipeStepDto> steps;
+	private List<IngredientDto> ingredients;
+	private List<RecipeNutrientDto> nutrients;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	private LocalDateTime deletedAt;
+}
