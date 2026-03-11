42d198747769390384c4d829ca7553ca421aa043
Yu <yukunchan1233@gmail.com>
2026-03-12 01:31:47 +0900

    feat: 프로필 조회용 UserProfileResponse 추가

--- src/main/java/com/dailytable/dailytable/domain/auth/UserProfileResponse.java
+++ src/main/java/com/dailytable/dailytable/domain/auth/UserProfileResponse.java
@@ -1,4 +1,19 @@
 package com.dailytable.dailytable.domain.auth;
 
-public class UserProfileResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class UserProfileResponse {
+        private String email;
+        private String nickname;
+        private String role;
+
+
 }
