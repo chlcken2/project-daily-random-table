4056359836c9f605a177ccbd89dbbc7766b15569
changhyeon <ckdgus988@gmail.com>
2026-03-13 23:31:03 +0900

    fix: 오타 수정

--- src/main/java/com/dailytable/dailytable/domain/recipe/RecipeController.java
+++ src/main/java/com/dailytable/dailytable/domain/recipe/RecipeController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/recipes")
-public class RecipeContoller {
+public class RecipeController {
 
     private final RecipeService recipeService;
 
