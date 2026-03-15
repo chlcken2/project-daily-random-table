ee601e17f6c9811bd2dfc5f85a896f3ccbcfd883
Yu <yukunchan1233@gmail.com>
2026-03-15 14:35:41 +0900

    feat: GachaMapper에 countTotalGenerations 추가 (가챠 총 횟수)

--- src/main/java/com/dailytable/dailytable/domain/gacha/GachaMapper.java
+++ src/main/java/com/dailytable/dailytable/domain/gacha/GachaMapper.java
@@ -6,5 +6,9 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface GachaMapper {
 
+    /** 당일 가챠 가능 횟수(당일 돌린 횟수) */
     int countTodayGenerations(@Param("userId") Long userId);
+
+    /** 가챠 총 횟수 (전체 누적) */
+    int countTotalGenerations(@Param("userId") Long userId);
 }
